524dfdbc8fea1b6ae55b7678bb62be9efa8b3aa9
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-19 08:46:02 +0530

    add Environment into DataSourcePropertyConfig, closes #2

--- src/main/java/spring/projects/di/config/DataSourcePropertyConfig.java
+++ src/main/java/spring/projects/di/config/DataSourcePropertyConfig.java
@@ -1,15 +1,20 @@
 package spring.projects.di.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import spring.projects.di.examplebeans.DataSource;
 
 @Configuration
 @PropertySource("classpath:dataSource.properties")
 public class DataSourcePropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${db.username}")
     private String username;
@@ -27,6 +32,6 @@ public class DataSourcePropertyConfig {
 
     @Bean
     public DataSource dataSource() {
-        return new DataSource(username, password, url);
+        return new DataSource(env.getProperty("USERNAME"), password, url);
     }
 }
